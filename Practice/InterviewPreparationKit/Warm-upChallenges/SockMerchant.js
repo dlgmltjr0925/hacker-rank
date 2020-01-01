176f0b11af7831634661d0fb124c603be7a461c1
heeseok lee <dlgmltjr0925@gmail.com>
2020-01-01 22:41:02 +0900

    resolve SockMerchant

---
+++ Practice/InterviewPreparationKit/Warm-upChallenges/SockMerchant.js
@@ -0,0 +1,61 @@
+/**
+ * https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ */
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+  inputString = inputString
+    .replace(/\s*$/, '')
+    .split('\n')
+    .map(str => str.replace(/\s*$/, ''));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+  let count = 0;
+  const pair = {};
+  for (let i = 0; i < n; i++) {
+    if (pair[ar[i]]) {
+      delete pair[ar[i]];
+      count++;
+    } else {
+      pair[ar[i]] = true;
+    }
+  }
+  return count;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine(), 10);
+
+  const ar = readLine()
+    .split(' ')
+    .map(arTemp => parseInt(arTemp, 10));
+
+  let result = sockMerchant(n, ar);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
