1d17d90301ca4642ffcd029c1f3e006fe62186c7
heeseok lee <dlgmltjr0925@gmail.com>
2020-01-01 22:41:21 +0900

    resolve CountingValleys

---
+++ Practice/InterviewPreparationKit/Warm-upChallenges/CountingValleys.js
@@ -0,0 +1,54 @@
+'use strict';
+/**
+ * www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ */
+
+https: const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+  inputString = inputString
+    .replace(/\s*$/, '')
+    .split('\n')
+    .map(str => str.replace(/\s*$/, ''));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+  let level = 0;
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    level += s[i] === 'U' ? 1 : -1;
+    if (level === 0 && s[i] === 'U') count++;
+  }
+  return count;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine(), 10);
+
+  const s = readLine();
+
+  let result = countingValleys(n, s);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
